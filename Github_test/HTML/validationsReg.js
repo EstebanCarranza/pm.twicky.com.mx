8c113e72f1ecd0e029bb79a9f1032ec27d932440
Aaron Ortega <33583011+detonator431@users.noreply.github.com>
2017-11-12 19:28:56 -0600

    Update and rename validationTel.js to validationsReg.js

--- Github_test/HTML/validationsReg.js
+++ Github_test/HTML/validationsReg.js
@@ -14,3 +14,15 @@ function validarcellphone(){
 		}
 	}
 }//function
+function equalpassword(){
+
+	var pass1 = document.getElementById("inputPassword").value;
+	var pass2 = document.getElementById("inputPassword2").value;
+
+	if(pass1==pass2){
+		document.getElementById("validatepass").innerHTML = " Contraseña Valida";
+	}else{
+		document.getElementById("validatepass").innerHTML = " Contraseña Invalida";
+	}
+
+}//function pass
