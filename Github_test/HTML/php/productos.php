1b347715c4b9ad28aaefa2a138e3ab66e19d2773
Aaron Ortega <33583011+detonator431@users.noreply.github.com>
2017-11-13 06:32:34 -0600

    Add files via upload
    cargar productos en combobox

---
+++ Github_test/HTML/php/productos.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'clases/modelo.php';
+
+		$datos = new BaseDatos();
+		$datos->abrir_conexion();
+		$result = $datos->dbquery("call sp_VerProductos ('',0,'','', 0, 10);");
+		
+		while($res = mysql_fetch_array($result, MYSQL_ASSOC))	
+		{
+			echo "<option>$res['descripcion']</option>";	
+		}
+
+		$datos->cerrar_conexion(true, $result);
+		
+		
+		/*echo "<option>hehexD</option>";
+		echo "<option>hahaxD</option>";*/
+?>
