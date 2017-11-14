2d34c3747ccc7db77fab6a4e0f08620d29cbc885
Aaron Ortega <33583011+detonator431@users.noreply.github.com>
2017-11-13 23:54:13 -0600

    Update validacionemail.php

--- Github_test/HTML/validacionemail.php
+++ Github_test/HTML/validacionemail.php
@@ -1,11 +1,31 @@
 <?php
+require_once 'php/clases/modelo.php';
 
-$email= $_POST['email'];
+	$email= $_POST['email'];
 
+	$datos = new BaseDatos();
+	$datos->abrir_conexion();
+	$result = $datos->dbquery("call sp_VerProspectos ('',0,'','', 0, 10);");
 
-if (filter_var($email, FILTER_VALIDATE_EMAIL) !== false) {
-  echo("Email valido");
-} else {
-  echo("Email invalido");
-}
+	if (filter_var($email, FILTER_VALIDATE_EMAIL) !== false) {
+		//echo "$email";
+		while($res = mysql_fetch_array($result, MYSQL_ASSOC))	
+			{
+				$correoDB = $res['correoElectronico'];
+				if($email==$correoDB)
+				{
+					echo "Email existente ingrese uno diferente";
+					break 1;
+				}//if
+				else
+				{
+					echo("Email valido");
+				}//else
+			}//while
+ 	
+	} else {
+	  echo("Email invalido");
+	}
+
+	$datos->cerrar_conexion(true, $result);
 ?> 
