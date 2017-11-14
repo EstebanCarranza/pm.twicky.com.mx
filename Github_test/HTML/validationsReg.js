e835cd24b968bc85832090dde8231d352727b321
Aaron Ortega <33583011+detonator431@users.noreply.github.com>
2017-11-14 01:55:38 -0600

    Update validationsReg.js

--- Github_test/HTML/validationsReg.js
+++ Github_test/HTML/validationsReg.js
@@ -1,19 +1,3 @@
-function validarcellphone(){
-	var valor = document.getElementById("inputCelular").value;
-	var NumberRegex = /^[0-9]*$/;
-
-	if(valor.length <= 10){
-	if(NumberRegex.test(valor)){
-	document.getElementById("validatetel").innerHTML = "Telefono Valido";
-	}else{
-		document.getElementById("validatetel").innerHTML = "Telefono Invalido";
-		}
-	}else{
-		if(valor.length >= 10){
-	document.getElementById("validatetel").innerHTML = "Telefono Invalido";
-		}
-	}
-}//function
 function equalpassword(){
 
 	var pass1 = document.getElementById("inputPassword").value;
