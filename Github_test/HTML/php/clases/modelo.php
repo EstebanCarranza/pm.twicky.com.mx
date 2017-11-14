c9ce3989c437407d83826a92002cbd9a5507409d
Esteban Carranza <esteban.carranza@outlook.com>
2017-11-13 23:16:01 -0600

    Auto stash before merge of "master" and "origin/master"

--- Github_test/HTML/php/clases/modelo.php
+++ Github_test/HTML/php/clases/modelo.php
@@ -23,13 +23,13 @@ class BaseDatos
 	public function abrir_conexion()
 	{
 		
-/*
-		$this->link = mysql_connect("localhost", "root", "root")
+
+		/*$this->link = mysql_connect("localhost", "root", "root")
 			or die('No se pudo conectar al servidor: ' . mysql_error());
 									
 		mysql_select_db("db_twicky_res") or die('No se pudo seleccionar la base de datos');
-*/
-		
+
+	*/
 		$this->link = mysql_connect("twicky.com.mx", "produccion_db", "produccion_db")
 			or die('No se pudo conectar al servidor: ' . mysql_error());
 									
