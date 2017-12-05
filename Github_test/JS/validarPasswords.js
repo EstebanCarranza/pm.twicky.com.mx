2ffa3727c7e1fe261b8d35c0281b38caf1d68988
Esteban Carranza <esteban.carranza@outlook.com>
2017-12-05 14:44:59 -0600

    Validaciones de password

--- Github_test/JS/validarPasswords.js
+++ Github_test/JS/validarPasswords.js
@@ -2,14 +2,13 @@ function validarPasswords(idInput1, idInput2)
 {
     var id1 = document.getElementById(idInput1).value;
     var id2 = document.getElementById(idInput2).value;
-    
+          
     if(id1 == id2)
     {
         return true;
     }
     else
     {
-        alert('Las contraseñas no coninciden');
         return false;
     }
     
